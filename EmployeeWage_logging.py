e2761ec8849bcefe7d0f37e172830da735c8e146
shital bramhe <shitalbramhe@gmail.com>
2022-02-16 16:33:44 +0530

    [Shital] add: part time employee wage ,use logger

--- EmployeeWage_logging.py
+++ EmployeeWage_logging.py
@@ -2,7 +2,7 @@
 @Author: Shital Bajait
 @Date: 2022-02-16 15:30:00
 @Last Modified by: Shital Bajait
-@Last Modified time: 2022-02-16 15:30:00
+@Last Modified time: 2022-02-16 16:30:00
 @Title : Check employee is present or absent
 '''
 import logging
@@ -22,11 +22,11 @@ logger.setLevel(logging.INFO)
 EMP_RATE_PER_HOUR = 20
 number = random.randint(0,1)
 if number == 1:
-    logger.info("Employee is present")
+    logger.info("Employee is FullTime")
     empHrs = 8
 else:
-    logger.info("Employee is absent")
-    empHrs = 0
-DaliyWage = empHrs * EMP_RATE_PER_HOUR
-logger.info("Daliy Wage is:{}".format(DaliyWage))
+    logger.info("Employee is PartTime")
+    empHrs = 4
+logger.info("Daliy Wage is : {}".format(empHrs * EMP_RATE_PER_HOUR))
+
     
