91eda2d9f583486089a666c3e91d4fcacaf1af2b
shital bramhe <shitalbramhe@gmail.com>
2022-02-16 18:21:06 +0530

    [Shital] add: store day and  daily wage using dictionary

--- EmployeeWage_logging.py
+++ EmployeeWage_logging.py
@@ -2,8 +2,8 @@
 @Author: Shital Bajait
 @Date: 2022-02-16 15:30:00
 @Last Modified by: Shital Bajait
-@Last Modified time: 2022-02-16 17:30:00
-@Title : Store Daily Wage
+@Last Modified time: 2022-02-16 17:54:00
+@Title : Store Day and Daily wage using Dictionary
 '''
 """
 Description:
@@ -31,31 +31,41 @@ EMP_RATE_PER_HOUR = 20
 NUM_OF_WORKING_DAYS = 20
 MAX_HRS_IN_MONTH = 100
 def FullTime():
+    """
+        Description:
+            Function to calculate hours
+        Parameter:
+            None
+        Return:
+            None
+    """
     return 8
 def PartTime():
     return 4
-def Absent():
-    return 0
 switcher = {
-        0: Absent(),
-        1: FullTime(),
-        2: PartTime(),
+        0: FullTime(),
+        1: PartTime(),
     }
 def total():
     totalEmpHrs = 0
     totalWorkingDays = 0
     totalEmpWage=0
-    empWage = []
-    while totalEmpHrs <= MAX_HRS_IN_MONTH and totalWorkingDays < NUM_OF_WORKING_DAYS:
-            number = random.randint(0,2)
+    empWage = {}
+    while totalEmpHrs < MAX_HRS_IN_MONTH and totalWorkingDays < NUM_OF_WORKING_DAYS:
+            number = random.randint(0,1)
             option = switcher.get(number)
+            if totalEmpHrs == 96 and option ==  8:
+                logger.info("you are working for more than 100 hours")
+                break
             totalEmpHrs=totalEmpHrs+option
-            empWage.append(option*EMP_RATE_PER_HOUR)
-            logger.info("Daily wage : {} ".format(empWage[totalWorkingDays]))
+            empWage[totalWorkingDays]=option * EMP_RATE_PER_HOUR
+            logger.info("Day : {} Daily wage : {}" .format(totalWorkingDays+1 , empWage[totalWorkingDays]))
             totalWorkingDays+=1
     logger.info("Total Employee Hours : {} ".format(totalEmpHrs))
-    logger.info("Total Employee Wage : {} ".format(sum(empWage)))
-
+    sum = 0
+    for i in empWage.values():
+           sum = sum + i
+    logger.info("Total Employee Wage : {} ".format(sum))
 
 if __name__ == "__main__":
     total()
