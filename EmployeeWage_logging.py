0caee29d74d77b470c02b1c331776c360816d73f
shital bramhe <shitalbramhe@gmail.com>
2022-02-16 16:53:18 +0530

    [Shital] add: total employee wage for month ,use logger

--- EmployeeWage_logging.py
+++ EmployeeWage_logging.py
@@ -2,8 +2,8 @@
 @Author: Shital Bajait
 @Date: 2022-02-16 15:30:00
 @Last Modified by: Shital Bajait
-@Last Modified time: 2022-02-16 16:30:00
-@Title : Check employee is present or absent
+@Last Modified time: 2022-02-16 16:51:00
+@Title : Calculate wage for a month
 '''
 import logging
 import random
@@ -19,26 +19,34 @@ logger = logging.getLogger()
 # Setting the threshold of logger to DEBUG
 logger.setLevel(logging.INFO)
 
-EMP_RATE_PER_HOUR = 20
-number = random.randint(0,2)
-def FullTime():
-    return 8
-def PartTime():
-    return 4
-def Absent():
-    return 0    
+EMP_RATE_PER_HOUR = 20 
+NUM_OF_WORKING_DAYS = 20
+empHrs = 0
+empWage = 0
+totalEmpWage=0
+day=0
+while day<NUM_OF_WORKING_DAYS:
+    number = random.randint(0,2)
+    def FullTime():
+        return 8
+    def PartTime():
+        return 4
+    def Absent():
+        return 0    
 
-switcher = {
-    0: Absent(),
-    1: FullTime(),
-    2: PartTime(),
-}
-# Get the option from switcher dictionary
-option = switcher.get(number)
+    switcher = {
+        0: Absent(),
+        1: FullTime(),
+        2: PartTime(),
+    }
+    # Get the option from switcher dictionary
+    option = switcher.get(number)
 
-empWage=option*EMP_RATE_PER_HOUR
-
+    empWage=option * EMP_RATE_PER_HOUR
+    totalEmpWage += empWage
+    day+=1
 #print employee wage
-logger.info("Employee Wage:{}".format(empWage))
+logger.info("Total Employee Wage : {} ".format(totalEmpWage))
+
 
     
