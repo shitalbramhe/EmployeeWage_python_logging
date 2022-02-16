21fed8d299d75937b5b8423858f4a4b70e5566c8
shital bramhe <shitalbramhe@gmail.com>
2022-02-16 17:37:43 +0530

    [Shital] add: total employee wage and hours, use function get work hours

--- EmployeeWage_logging.py
+++ EmployeeWage_logging.py
@@ -3,8 +3,16 @@
 @Date: 2022-02-16 15:30:00
 @Last Modified by: Shital Bajait
 @Last Modified time: 2022-02-16 16:51:00
-@Title : Calculate wage for a month hours reached maximum 
+@Title : Refactor code using function
 '''
+"""
+Description:
+    Function to calculate hours
+Parameter:
+      None
+Return:
+      None
+"""
 import logging
 import random
  
@@ -18,38 +26,39 @@ logger = logging.getLogger()
  
 # Setting the threshold of logger to DEBUG
 logger.setLevel(logging.INFO)
-EMP_RATE_PER_HOUR = 20 
+EMP_RATE_PER_HOUR = 20
 NUM_OF_WORKING_DAYS = 20
-empWage = 0
-totalEmpWage=0
-totalEmpHrs = 0
-totalWorkingDays = 0
-day=0
 MAX_HRS_IN_MONTH = 100
-while totalEmpHrs <= MAX_HRS_IN_MONTH and totalWorkingDays < NUM_OF_WORKING_DAYS:
-    totalWorkingDays+=1
-    number = random.randint(0,2)
-    def FullTime():
-        return 8
-    def PartTime():
-        return 4
-    def Absent():
-        return 0    
-
-    switcher = {
+def FullTime():
+    return 8
+def PartTime():
+    return 4
+def Absent():
+    return 0
+switcher = {
         0: Absent(),
         1: FullTime(),
         2: PartTime(),
     }
-    # Get the option from switcher dictionary
-    option = switcher.get(number)
-    totalEmpHrs+=option
-    empWage=option*EMP_RATE_PER_HOUR
-    totalEmpWage += empWage
+def total():
+    totalEmpHrs = 0
+    totalWorkingDays = 0
+    totalEmpWage=0
+    empWage = 0
+    while totalEmpHrs <= MAX_HRS_IN_MONTH and totalWorkingDays < NUM_OF_WORKING_DAYS:
+            totalWorkingDays+=1
+            number = random.randint(0,2)
+            option = switcher.get(number)
+            totalEmpHrs=totalEmpHrs+option
+            empWage=option * EMP_RATE_PER_HOUR
+            totalEmpWage += empWage
+    logger.info("Total Employee Hours : {} ".format(totalEmpHrs))
+    logger.info("Total Employee Wage : {} ".format(totalEmpWage))
 
-#print employee wage
-logger.info("Total Employee Hours : {} ".format(totalEmpHrs))
-logger.info("Total Employee Wage : {} ".format(totalEmpWage))
+
+if __name__ == "__main__":
+    total()
+
 
 
 
