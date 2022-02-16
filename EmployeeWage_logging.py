74231657744b1e67cdb71961887fd1220752edd2
shital bramhe <shitalbramhe@gmail.com>
2022-02-16 16:27:38 +0530

    [Shital] add: Calculate daily wage , use  logger

--- EmployeeWage_logging.py
+++ EmployeeWage_logging.py
@@ -1,8 +1,8 @@
 '''
 @Author: Shital Bajait
-@Date: 2022-02-16 15:20:00
+@Date: 2022-02-16 15:30:00
 @Last Modified by: Shital Bajait
-@Last Modified time: 2022-02-16 15:20:00
+@Last Modified time: 2022-02-16 15:30:00
 @Title : Check employee is present or absent
 '''
 import logging
@@ -19,8 +19,14 @@ logger = logging.getLogger()
 # Setting the threshold of logger to DEBUG
 logger.setLevel(logging.INFO)
 
+EMP_RATE_PER_HOUR = 20
 number = random.randint(0,1)
 if number == 1:
     logger.info("Employee is present")
+    empHrs = 8
 else:
     logger.info("Employee is absent")
+    empHrs = 0
+DaliyWage = empHrs * EMP_RATE_PER_HOUR
+logger.info("Daliy Wage is:{}".format(DaliyWage))
+    
